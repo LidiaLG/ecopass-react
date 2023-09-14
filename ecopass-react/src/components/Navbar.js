f15f86e11ca2d41f2981f0e8cac93390bd7d9fee
LidiaLG <lidialuquegallego91@gmail.com>
2023-09-14 19:09:37 +0200

    arreglado logo y desplegable

--- ecopass-react/src/components/Navbar.js
+++ ecopass-react/src/components/Navbar.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import './Navbar.css';
-import Logo from './../assets/logo.svg'
-
-/**Cuando hago click en .button, la clase .nav activa el TOGGLE 'activo' */
-const menu = document.querySelector('.menu');
-const nav    = document.querySelector('.nav');
-    
-menu.addEventListener('click', () => {
-    nav.classList.toggle('activo')
-})
+import Logo from './../assets/logo.svg';
+import { useEffect } from 'react';
 
 const Navbar = () => {
+    /**Cuando hago click en .button, la clase .nav activa el TOGGLE 'activo' */
+    useEffect(() => {
+        const menu = document.querySelector('.menu');
+        const nav = document.querySelector('.nav');
+       
+        menu.addEventListener('click', () => {
+          nav.classList.toggle('activo')
+        });
+      }, []);
   return (
     <header class="header">
 
     <button class="menu">
-        {/* <svg class="svg" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+         <svg class="svg" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-          </svg> */}
-          {Logo}
+          </svg> 
+          
+
     </button>
 
     <a href="#" class="a">
-        <img class="logo" src="./../assets/logo.svg" alt="logo"/>
+        <img className="logo" src={Logo} alt="logo" />
     </a>
 
     <nav class="nav">
