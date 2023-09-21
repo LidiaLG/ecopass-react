fd70fa061f1094c04e840e15823c632e7748ca76
Eugenia Cames <eugenia23@pluralcamp.com>
2023-09-21 12:24:54 +0200

    componente misión y visión con CSS corregido

--- ecopass-react/src/App.js
+++ ecopass-react/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import MisionYVision from './components/Misionyvision';
 
 function App() {
   return (
+    <div className="App">
     <Navbar></Navbar>
+    <MisionYVision/>
+    </div>
   );
 }
 
---
+++ ecopass-react/src/components/Misionyvision.js
@@ -0,0 +1,36 @@
+import React from "react" //esto para qué es?
+import "./MisionYVision.css"
+
+function MisionYVision() {
+
+    return (
+        <><div className="row">
+            <div className="divider"></div>
+        </div>
+            <div className="title">
+                <div className="titlesection">
+                    <h1>MISIÓN Y VISIÓN</h1>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="divider"></div>
+                </div>
+                <div className="row" id="info"></div>
+                <div className="boxmyv">
+                    <p className="infotitle">Nuestra Misión es promover la cultura y la conciencia ecológica en los jóvenes</p><br></br>
+                    <p className="info">Queremos promover el acceso a la cultura y conciencia ecológica. Brindar a los adolescentes entre 14 y 18 años la oportunidad de experimentar y cultivar su sensibilidad cultural y su conciencia ecológica. Buscamos crear un espacio donde los jóvenes puedan participar en actividades culturales enriquecedoras de manera gratuita, al tiempo que fomentamos una comprensión más profunda y respetuosa hacia el entorno que nos rodea. A través de una amplia gama de eventos y experiencias, aspiramos a cultivar la creatividad, el pensamiento crítico y la colaboración entre los jóvenes, al mismo tiempo que los inspiramos a desempeñar un papel activo en la preservación de nuestro planeta. Creemos que al empoderar a los jóvenes con acceso a la cultura y alentando la conciencia ecológica desde una edad temprana, estamos forjando líderes conscientes y responsables que darán forma a un futuro más sostenible y armonioso</p>
+                </div>
+        <div className="divider"></div>
+                <div className="boxmyv">
+                    <p className="infotitle">Visión: inspirar a los jóvenes ciudadanos del mundo</p><br></br>
+                    <p className="info">Aspiramos a crear un movimiento juvenil que trascienda las fronteras y las limitaciones, nutriendo a jóvenes ciudadanos del mundo que están profundamente arraigados en su propia cultura y sensibles a la diversidad cultural de los demás. Visualizamos una generación de jóvenes empoderados que se sienten conectados con el patrimonio cultural y natural de su entorno, al tiempo que reconocen su papel como guardianes de un planeta compartido. Imaginamos un futuro en el que los jóvenes, a través de su participación en nuestras actividades, desarrollen una comprensión intrínseca de la importancia de la cultura y la sostenibilidad. Esta comprensión no solo enriquecerá sus vidas personales, sino que también inspirará a otros y sentará las bases para una sociedad más consciente y armoniosa en las décadas venideras. En última instancia, nuestra visión es ver a estos jóvenes crecer y convertirse en líderes que promuevan la cultura, la conciencia ecológica y el entendimiento global en sus comunidades y más allá. Estamos comprometidos a ser catalizadores de este proceso, brindando oportunidades que nutran su crecimiento personal y contribuyan positivamente a la sociedad en su conjunto.</p>
+                </div>
+            </div>
+            <div className="row">
+            <div className="divider"></div>
+        </div></>
+    )
+}
+
+export default MisionYVision
