3986740782502fbe1cbf26c38ccf4892bd184482
LidiaLG <lidialuquegallego91@gmail.com>
2023-09-21 10:08:23 +0200

    login añadido

--- ecopass-react/src/App.js
+++ ecopass-react/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './components/Footer';
+import Login from './components/Login';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Login></Login>
       <Footer></Footer>
     </>
   );
