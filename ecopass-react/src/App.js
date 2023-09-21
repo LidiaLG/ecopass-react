843eff7280732697bcc407fd3da82109059706c6
Eugenia Cames <eugenia23@pluralcamp.com>
2023-09-21 13:52:52 +0200

    modal usuario registrado, pendiente centrarlo

--- ecopass-react/src/App.js
+++ ecopass-react/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import MisionYVision from './components/Misionyvision';
+import RegisterModal from './components/RegisterModal';
 
 function App() {
   return (
     <div className="App">
     <Navbar></Navbar>
     <MisionYVision/>
+    <RegisterModal/>
     </div>
   );
 }
