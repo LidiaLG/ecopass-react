4a94a4aae15cace26d80734492b333bad937e181
LidiaLG <lidialuquegallego91@gmail.com>
2023-09-24 12:04:16 +0200

    vista faq acabada

--- ecopass-react/src/App.js
+++ ecopass-react/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import Footer from './components/Footer';
-import MisionVision from './components/MisionVision';
-// import Login from './components/Login';
+// import MisionVision from './layouts/MisionVision';
+// import Login from './layouts/Login';
 import Navbar from './components/Navbar';
+import Faq from './layouts/Faq';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       {/* <Login></Login> */}
-      <MisionVision></MisionVision>
+      {/* <MisionVision></MisionVision> */}
+      <Faq></Faq>
       <Footer></Footer>
     </>
   );
