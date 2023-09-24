26962abcdd06ee6bcbcab5a1a0a1e1f574ce4050
LidiaLG <lidialuquegallego91@gmail.com>
2023-09-24 12:21:55 +0200

    vista terminos y condiciones acabada

--- ecopass-react/src/App.js
+++ ecopass-react/src/App.js
@@ -3,7 +3,8 @@ import Footer from './components/Footer';
 // import MisionVision from './layouts/MisionVision';
 // import Login from './layouts/Login';
 import Navbar from './components/Navbar';
-import Faq from './layouts/Faq';
+import TerminosYCondiciones from './layouts/TerminosYCondiciones';
+// import Faq from './layouts/Faq';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Navbar></Navbar>
       {/* <Login></Login> */}
       {/* <MisionVision></MisionVision> */}
-      <Faq></Faq>
+      {/* <Faq></Faq> */}
+      <TerminosYCondiciones></TerminosYCondiciones>
       <Footer></Footer>
     </>
   );
