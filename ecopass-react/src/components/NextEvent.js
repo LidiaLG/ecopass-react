8760c70caddc712bb5b6ec3d3f205acddd0afcf9
Lina Ramirez <ramirezlina215@gmail.com>
2023-10-11 08:29:36 +0200

    Conectar Backend

--- ecopass-react/src/components/NextEvent.js
+++ ecopass-react/src/components/NextEvent.js
@@ -4,24 +4,42 @@ import './main.css'
 
 export default function CurrentEvent(){
 
-const [events, setEvents] = useState([]);
-
-    useEffect(()=>{
-        fetch('https://018123df-1a58-480a-a0b1-74bef2cb6e76.mock.pstmn.io/api/events')
-        .then((res) => res.json())   
-        .then (data => {
-            setEvents(data)        
-        })  
-    }, [])
-
-    return(
-
-        events.map( item=> {
-            if (item.tipo === "evento proximo" && item.categoria === "recompensa"){
-                
-                return<Card event_img={item.imagen} event_name={item.nombre} localitation={item.ubicacion} event_date={item.fecha} event_desc={item.descripcion}/>
+    
+    const [events, setEvents] = useState([]);
+    const [error, setError] = useState(null);
+    
+    useEffect(() => {
+        fetch('http://44.215.37.34/eventoactual')
+        .then((res) => {
+            if (!res.ok) {
+            throw new Error('Network response was not ok');
             }
-        
-})
-    )
-}
+            return res.json();
+        })
+        .then((data) => {
+            setEvents(data);
+        })
+        .catch((error) => {
+            setError(error);
+        });
+    }, []);
+    
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+    
+    return (
+        <div>
+        {events.map((item) => (
+            <Card
+            event_img={item.imagen}
+            event_name={item.name}
+            localitation={item.location}
+            event_date={item.date}
+            event_desc={item.description}
+            />
+        ))}
+        </div>
+    );
+    }
+    
