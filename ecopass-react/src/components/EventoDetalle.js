7313e24e7ff34db89b29214bcd1570d42b6824fe
Lina Ramirez <ramirezlina215@gmail.com>
2023-10-11 09:41:41 +0200

    galeria eventos

---
+++ ecopass-react/src/components/EventoDetalle.js
@@ -0,0 +1,14 @@
+import './App.css';
+import EventList from './componentes/cardEventAnt';
+import Galery from './componentes/Galery';
+
+function App() {
+  return (
+    <div className="App">
+      <EventList/>
+      <Galery/>
+    </div>
+  );
+}
+
+export default App;
